70a317633c0dd974ab2e21ce5e5ddedf05fd12d1
Lachlan Garrity <lachlangarrity@gmail.com>
2023-09-05 16:36:20 +1000

    Add project files.

---
+++ RadixSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RadixSort;
+class Program
+{
+    static void Main(string[] args)
+    {
+        RandomArray randomArray = new RandomArray();
+
+        int[] intArray = randomArray.newRandomIntArray(10, 0, 200);
+        Console.WriteLine(randomArray.ArrayToString(intArray));
+    }
+}
---
+++ RadixSort/RandomArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadixSort
+{
+    public class RandomArray
+    {
+        public int[] newRandomIntArray(int size, int min, int max)
+        {
+            int[] intArray = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = rnd.Next(min, max);
+            }
+            return intArray;
+        }
+        public String ArrayToString(int[] array)
+        {
+            String arrayString = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > 0) { arrayString = arrayString + ", "; }
+                arrayString = arrayString + array[i];
+            }
+
+            return arrayString;
+        }
+    }
+}
