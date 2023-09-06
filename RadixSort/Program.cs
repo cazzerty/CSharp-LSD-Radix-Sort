9b65f6032e95bf726217aa442f023a349d365612
Lachlan Garrity <lachlangarrity@gmail.com>
2023-09-06 22:56:27 +1000

    Added Execution Time

--- RadixSort/Program.cs
+++ RadixSort/Program.cs
@@ -5,7 +5,7 @@ class Program
     {
         ArrayManager arrayManager = new ArrayManager();
         RadixSort radixSort = new RadixSort();
-        
+
         /*
         int[] intArray = arrayManager.newRandomIntArray(10, 0, 200);
         Console.WriteLine(arrayManager.ArrayToString(intArray));
@@ -14,18 +14,24 @@ class Program
         intArray = arrayManager.ReverseArray(intArray);
         Console.WriteLine(arrayManager.ArrayToString(intArray));
         */
-        
+
+
+        var watch = new System.Diagnostics.Stopwatch();
 
         Console.WriteLine("_____________________________________________");
-        int[] radixArray = arrayManager.newRandomIntArray(100, 1, 999999);
+        int[] radixArray = arrayManager.newRandomIntArray(1000000, 1, 999999);
 
-        radixArray = arrayManager.CreateAscendingArray(999);
+        watch.Start();
+        //radixArray = arrayManager.CreateAscendingArray(999);
         radixArray = arrayManager.ShuffleArray(radixArray);
 
         //Console.WriteLine(arrayManager.ArrayToString(radixArray));
 
 
         radixArray = radixSort.Lsd_RadixSort(radixArray);
+        watch.Stop();
+
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
         //Console.WriteLine(arrayManager.ArrayToString(radixArray));
         Console.WriteLine("Array Sorted?: " + arrayManager.CheckIfSorted(radixArray));
         
