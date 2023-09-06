3f0e539aa2403206f801d046fccf682058e7e7d7
Lachlan Garrity <lachlangarrity@gmail.com>
2023-09-06 13:24:02 +1000

    find largest

--- RadixSort/radixSort.cs
+++ RadixSort/radixSort.cs
@@ -10,7 +10,13 @@ namespace RadixSort
     {
         public int[] lsd_radixSort(int[] nums)
         {
-            if(nums.Length() <= 1) { return; }
+            if (nums.Length < 2) return nums;
+            //Find the largest element in the array
+            int largest = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > largest) { largest = nums[i]; }
+            }
             return nums;
         }
     }
